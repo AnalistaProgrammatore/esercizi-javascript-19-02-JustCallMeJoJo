fbb20e72a48f07729cec2259f40d054849b664ee
JustCallMeJoJo <59923599+JustCallMeJoJo@users.noreply.github.com>
2020-02-25 14:35:17 +0100

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,15 @@
 /**
 * Scrivere qui il codice della funzione fibonacci(n)
 **/
+
+function fibonacci(x) {
+  if (x <= 1) {
+    return x;
+  } else {
+    return fibonacci(x - 1) + fibonacci(x - 2);
+  }
+}
+
+for (let n = 0; n < 30; n++) {
+  console.log(fibonacci(n));
+}
