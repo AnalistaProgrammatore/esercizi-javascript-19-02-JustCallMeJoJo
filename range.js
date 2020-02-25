dd24235410ab099fff768ad71867231611665d7f
JustCallMeJoJo <59923599+JustCallMeJoJo@users.noreply.github.com>
2020-02-25 14:57:28 +0100

    Update range.js

--- range.js
+++ range.js
@@ -1,3 +1,14 @@
 /**
 * Scrivere qui il codice dell'esercizio 1 del capitolo 4 di eloquentJS
 **/
+
+const x = function range(start, end) {
+  let arrayRange = [];
+  while (start <= end) {
+    arrayRange.push(start);
+    start++;
+  }
+  return arrayRange;
+};
+let y = x(4, 15);
+console.log(y);
