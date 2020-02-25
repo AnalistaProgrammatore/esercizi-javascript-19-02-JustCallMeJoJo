a12a7cdc24e2e5b90bee4823ae221fba56cb6a4d
JustCallMeJoJo <59923599+JustCallMeJoJo@users.noreply.github.com>
2020-02-25 14:38:33 +0100

    Update mcd.js

--- mcd.js
+++ mcd.js
@@ -1,3 +1,13 @@
 /**
 * Scrivere qui il codice delle funzioni mcd(x, y) e mcdRecursive(x, y)
 **/
+
+function mcd(x, y) {
+  while (y != 0) {
+    let z = x % y;
+    x = y;
+    y = z;
+  }
+  return x;
+}
+console.log(mcd(3, 12));
